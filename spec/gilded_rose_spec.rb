4d71a71b90d7c3de3a7f4140de2775cd6aa63c6b
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-12-28 16:23:13 +0000

    When the quality of the backstage pass is already 50 and there are only 10 days left until the concert, the quality will not change.

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -91,6 +91,14 @@ describe GildedRose do
       it 'increases in quality by 2' do
         expect { update_quality }.to change { backstage_pass.quality }.by 2
       end
+
+      context 'and it already has a quality of 50' do
+        let(:backstage_pass) { a_backstage_pass(sell_in: 10, quality: 50) }
+        
+        it 'does not change in quality' do
+          expect { update_quality }.not_to change { backstage_pass.quality }
+        end
+      end
     end
   end
 
