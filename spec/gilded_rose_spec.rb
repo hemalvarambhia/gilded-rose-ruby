6a531bc614945814d72d19dedd29b10e36051094
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-12-28 14:49:36 +0000

    A backstage pass that already has a quality of 50, does not change in quality on update.

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -59,6 +59,14 @@ describe GildedRose do
       it 'increases in quality by 1' do
         expect { update_quality }.to change { backstage_pass.quality }.by 1
       end
+
+      context 'and the quality is already 50' do
+        let(:backstage_pass) { a_backstage_pass(quality: 50) }
+        
+        it 'does not change in quality' do
+          expect { update_quality }.not_to change { backstage_pass.quality }
+        end
+      end
     end
   end
 
