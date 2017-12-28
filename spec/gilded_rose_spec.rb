4739d5f40af6c6e1351d0fa7a560ae939972d20d
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-12-28 10:42:48 +0000

    Wrote up a test list for updating the quality of aged brie.

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,12 +1,21 @@
 require_relative File.join('..', 'lib', 'gilded_rose')
 
 describe GildedRose do
+  describe 'Aged Brie' do
+    context 'when it is not passed its sell-by date' do
+      it 'increases in quality by 1'
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      context 'and the quality is already 50' do
+        it 'does not change in quality'
+      end
+    end
+
+    context 'when it hits its sell-by date' do
+      it 'increases in quality by 2'
+    end
+
+    context 'when it has passed its sell-by date' do
+      it 'increases in quality by 2'
     end
   end
 
